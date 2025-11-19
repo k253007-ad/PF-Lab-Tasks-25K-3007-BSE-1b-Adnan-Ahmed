53b6ca8aa871a7f4cf5cd841deab2a6c668daac5
Adnan_Ahmed_25K-3007 <k253007@nu.edu.pk>
2025-11-19 19:33:11 +0500

    Update Question7.c

--- Lab-10/Question7.c
+++ Lab-10/Question7.c
@@ -23,7 +23,7 @@ int main() {
     
     while ( run != 0 ){
         
-        n = -
+        n = 0;
         
         printf("\n\nType 1 : to Check Students Courses");
         printf("\nType 2 : to Check Students in Specific Course");
@@ -36,7 +36,7 @@ int main() {
             for(i = 0; i < 5 ; i++){
                 printf("\n%-8s :", students[i]);
                 for(j = 0; j < 5; j++){
-                    if(strcmp(courses[i][j],"NO")) printf("%-4s", courses[i][j]);
+                    if(strcmp(courses[i][j],"NO") printf("%-4s", courses[i][j]);
                 }
             }
             printf("\n-------------------------");
